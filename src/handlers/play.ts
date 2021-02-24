82d3c86125e7e68bb0fded37f6aa255d19708df3
Kermitofx <39018448+Kermitofx@users.noreply.github.com>
2021-02-24 20:05:48 -0300

    Update play.ts

--- src/handlers/play.ts
+++ src/handlers/play.ts
@@ -5,7 +5,7 @@ export const playHandler = Composer.command('play', async ctx => {
     const { chat } = ctx.message;
 
     if (chat.type !== 'supergroup') {
-        await ctx.reply('I can only play in groups.');
+        await ctx.reply('Eu só posso tocar em grupos.');
         return;
     }
 
@@ -13,11 +13,11 @@ export const playHandler = Composer.command('play', async ctx => {
     const text = ctx.message.text.slice(commandEntity.length + 1);
 
     if (!text) {
-        await ctx.reply('You need to specify a YouTube URL.');
+        await ctx.reply('Você precisa especificar um URL do YouTube.');
         return;
     }
 
     const index = await addToQueue(chat, text);
 
-    await ctx.reply(index === 0 ? 'Playing.' : `Queued at position ${index}.`);
+    await ctx.reply(index === 0 ? 'Tocando...' : `Queued at position ${index}.`);
 });
